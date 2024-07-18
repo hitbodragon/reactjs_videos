115d24b08444a13414694eb98a3f9a0381d0f55a
hitbodragon <longtrong02@mail.com>
2024-07-18 22:41:00 +0700

    cài đặt babel-plugin-model-resolver

---
+++ src/alert.js
@@ -0,0 +1,8 @@
+
+
+function Alert(){
+    return(
+        alert(" Thông tin của bạn là gì")
+    )
+}
+export default Alert;
---
+++ src/component/Button/index.js
@@ -0,0 +1,9 @@
+
+import myAlert from '~/alert'
+
+function Button(){
+    return (
+        <button onClick={myAlert}>Click Me!</button>
+    )
+}
+export default Button
