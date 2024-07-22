7e6e4919efb9b4e64538af8c16f9938255d9a5f5
hitbodragon <longtrong02@mail.com>
2024-07-22 17:49:04 +0700

    Xây dựng ui header

--- src/component/Layout/components/Header/index.js
+++ src/component/Layout/components/Header/index.js
@@ -1,12 +1,33 @@
+import React from 'react';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
-// cài thư viện  npm i classnames
-// bind có tác dụng bind cái object của styles lại cho cx
+import { AiTwotoneCloseCircle, AiOutlineSearch, AiOutlineLoading } from 'react-icons/ai';
+// import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+// import { faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import images from '~/assets/images';
+
 const cx = classNames.bind(styles);
+
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <AiTwotoneCloseCircle />
+                    </button>
+                    <AiOutlineLoading className={cx('loading')} />
+
+                    <button className={cx('search-btn')}>
+                        <AiOutlineSearch />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
         </header>
     );
 }
