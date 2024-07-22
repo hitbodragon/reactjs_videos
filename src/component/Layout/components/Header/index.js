e90ec0e2fab72957a6b43a5f90a9207fdb44b671
hitbodragon <longtrong02@mail.com>
2024-07-22 11:06:28 +0700

    dựng khung layout mặt định , npm i classnames trong scss

--- src/component/Layout/components/Header/index.js
+++ src/component/Layout/components/Header/index.js
@@ -1,5 +1,14 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+// cài thư viện  npm i classnames
+// bind có tác dụng bind cái object của styles lại cho cx
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2> Header </h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    );
 }
 
 export default Header;
